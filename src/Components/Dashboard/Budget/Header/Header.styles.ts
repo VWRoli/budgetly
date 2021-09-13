31a18f12ccc98d4d59c07d0913fb6d670575a272
VWRoli <fustroli@gmail.com>
2021-09-13 16:55:20 +0200

    Restart layout and design plans

--- src/Components/Dashboard/Budget/Header/Header.styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Wrapper = styled.div`
-  background-color: var(--light-clr);
-  padding: 0.25rem 0.75rem;
-  margin-bottom: 0.75rem;
-  display: flex;
-  justify-content: space-between;
-`;
--- src/Components/Dashboard/Transactions/Transactions.tsx
+++
@@ -1,7 +0,0 @@
-import TransactionsContainer from './Transactions.styles';
-
-const Transactions: React.FC = (): JSX.Element => (
-  <TransactionsContainer>Transactions</TransactionsContainer>
-);
-
-export default Transactions;
