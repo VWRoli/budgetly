c16a346e3a67d462bb0ee2c920191be349e53525
VWRoli <fustroli@gmail.com>
2021-12-13 17:04:40 +0100

    feat: issue #18 improve progress bar

--- src/components/Dashboard/Budget/BudgetItem.tsx
+++ src/components/Dashboard/Budget/BudgetItem.tsx
@@ -1,6 +1,7 @@
+import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 //Components
 import Chip from 'src/components/common/Chip/Chip';
-import ProgressBar from 'src/components/common/ProgressBar/ProgressBar';
 
 const BudgetItem: React.FC = (): JSX.Element => {
   return (
@@ -12,7 +13,17 @@ const BudgetItem: React.FC = (): JSX.Element => {
         <p>Spent</p>
       </div>
       <div className="progress-wrapper">
-        <ProgressBar />
+        <CircularProgressbar
+          value={65}
+          text={`${65}%`}
+          styles={buildStyles({
+            strokeLinecap: 'round',
+            textSize: '24px',
+            pathColor: `#8c4af2e5`,
+            textColor: '#14111e',
+            trailColor: '#d6d6d6',
+          })}
+        />
       </div>
     </div>
   );
