c9102ac46e8eecb880b7762d8378487c6c6ea8a9
VWRoli <fustroli@gmail.com>
2021-12-22 16:52:33 +0100

    chore: install prettyquick

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,8 +3,8 @@ module.exports = {
   '*': ['prettier --ignore-unknown --write'],
 
   // lint javascript after formatting
-  '*.{ts,tsx}': ['eslint --fix --cache --quiet'],
+  '*.{ts,tsx}': ['eslint --fix src'],
 
   // lint entire project if eslint settings changed
-  '.eslint*': ['eslint src --cache'],
+  '.eslint*': ['eslint src'],
 };
